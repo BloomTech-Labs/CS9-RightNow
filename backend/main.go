49adb0118a1deadad0b58eee72b1c5021003fc9d
Henry K <mykim1891@hotmail.com>
2018-08-15 12:31:42 -0700

    Switching from Auth0 to different library;

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-    // "encoding/json"
-    "github.com/gorilla/mux"
-    // "log"
-    "net/http"
-    // "strings"
-    // "github.com/rs/cors"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+	"encoding/json"
+	// "fmt"
+	"os"
+	"github.com/dgrijalva/jwt-go"
+	// "log"
+  // "strings"
+  // "github.com/rs/cors"
 )
 
 // Basic user by id
@@ -26,7 +30,7 @@ import (
 
 // user appointments by id
 type Appointment struct {
-	Id int
+	App_Id int
 	Location string
 	Company string
 	Time string // string for now
@@ -36,11 +40,47 @@ type Appointment struct {
 
 // creating list of Appointments
 var appointments = []Appointment{
-	Appointment(Id: 1, Location: "Some location 1", Company: "James May", Time: "some time 1", Booking: true, User_id: 1),
-	Appointment(Id: 2, Location: "Some location 2", Company: "Richard Hammond", Time: "some time 2", Booking: true, User_id: 1),
-	Appointment(Id: 1, Location: "Some location 3", Company: "Jeremy Clarkson", Time: "some time 3", Booking: true, User_id: 1),
-	Appointment(Id: 1, Location: "Some location 4", Company: "Top Gear Team", Time: "some time 4", Booking: true, User_id: 1),
+	Appointment{App_Id: 1, Location: "Some location 1", Company: "James May", Time: "some time 1", Booking: true, User_id: 1},
+	Appointment{App_Id: 2, Location: "Some location 2", Company: "Richard Hammond", Time: "some time 2", Booking: true, User_id: 1},
+	Appointment{App_Id: 3, Location: "Some location 3", Company: "Jeremy Clarkson", Time: "some time 3", Booking: true, User_id: 1},
+	Appointment{App_Id: 4, Location: "Some location 4", Company: "Top Gear Team", Time: "some time 4", Booking: true, User_id: 1},
 }
+
+// invoked when user calls /status route; confirms API running
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("API is up and running")) // confirm API running for now.
+})
+
+var AppointmentHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	// convert the slice to JSON format
+	payload, _ := json.Marshal(appointments)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
+})
+
+// Feedback request; for now we will just look for request OK status, when appointment is matched.
+var AddFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	var appointment Appointment
+	vars := mux.Vars(r)
+	slug := vars["slug"] // looks through the slug part of url
+	println(slug)
+
+	for _, p := range appointments {
+		if p.App_Id == slug {
+			appointment = p
+		}
+	}
+
+	w. Header().Set("Content-Type", "application/json")
+	if appointment.App_Id != 0 {
+	payload, _ := json.Marshal(appointment)
+	w.Write([]byte(payload))
+	} else {
+		w.Write([]byte("Appointment Not Found"))
+	}
+})
+
 
 //Get all companies
 // func GetCompanies(w http.ResponseWriter, r *http.Request) {
@@ -86,10 +126,13 @@ func main() {
 		/status - checks if the API is up and running
 		/products - retrieve list of products
 		/products/{slug}/feedback - capture feedback ** for purpose of practicing Golang
+		/get-token - simply generates token
 		*/
-		 r.Handle("/status", NotImplemented).Methods("GET")
-		 r.Handle("/products", NotImplemented).Methods("GET")
-		 r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+		 r.Handle("/status", StatusHandler).Methods("GET")
+		 r.Handle("/products", AppointmentHandler).Methods("GET")
+		 r.Handle("/products/{slug}/feedback", AddFeedbackHandler).Methods("POST")
+
+		 r.Handle("/get-token", GetTokenHanlder).Methods("GET")
 		 
 		 r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
@@ -103,9 +146,31 @@ func main() {
     
 		// handler := c.Handler(router)
 		
-		http.ListenAndServe(":5000", r)
-    // log.Fatal(http.ListenAndServe(":5000", loggingMiddleware(handler)))
+		// logging handler is wrapped around in our router; logger is now called first on each route request.
+    http.ListenAndServe(":5000", handlers.LoggingHandler(os.Stdout, r))
 }
+
+// global string for my secret
+var mySigningKey = []byte("secret")
+
+// Handlers
+var GetTokenHanlder = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Create a map to store our claims
+	claims := token.Claims.(jwt.MapClaims)
+	
+	// set token claims: avoid this hard-coded stuff.
+	claims["name"] = "Captain Slow"
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	// sign token with my secret
+	tokenString, _ := token.SignedString(mySigningKey)
+
+	// write token to browser window; for now
+	w.Write([]byte(tokenString))
+})
 
 // Simply displays "Not Implemented" whenever endpoint with this function is hit.
 var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
