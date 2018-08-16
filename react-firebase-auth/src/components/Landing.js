aef438e71a56e0d3660c66edfa3a35f06ebd9c2f
Mark Marchant <jtla3@yahoo.com>
2018-08-16 10:13:48 -0700

    boiler plate set up for react & firebase with auth

---
+++ react-firebase-auth/src/components/Landing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LandingPage = () =>
+    <div>
+        <h1>Landing</h1>
+    </div>
+
+export default LandingPage;
---
+++ react-firebase-auth/src/firebase/auth.js
@@ -0,0 +1,23 @@
+import { auth } from './firebase';
+
+// Sign Up
+export const doCreateUserWithEmailAndPassword = (email, password) =>
+    auth.createUserWithEmailAndPassword(email, password);
+
+// Sign In
+export const doSignInWithEmailAndPassword = (email, password) =>
+    auth.signInWithEmailAndPassword(email, password);
+
+// Sign Out
+// You don’t need to pass any argument to it, 
+// because the auth object itself knows about the currently authenticated user(if a user is authenticated in the first place).
+export const doSignOut = () =>
+    auth.signOut();
+
+// Password Reset
+export const doPasswordReset = (email) =>
+    auth.sendPasswordResetEmail(email);
+
+// Password Change
+export const doPasswordUpdate = (password) =>
+    auth.currentUser.updatePassword(password);
