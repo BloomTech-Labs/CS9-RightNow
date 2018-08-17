54632ceafc1bcd5343d5f453d2d90d17ac28a9c9
harrisonbrock <harrison@harrisonbrock.com>
2018-08-17 18:22:23 -0400

    Added business component

--- rightnow-firebase/src/App.js
+++ rightnow-firebase/src/App.js
@@ -30,23 +30,6 @@ class App extends Component {
 				<NavBar/>
 				<UserLanding/>
 				<AppointmentContainer/>
-				{/*<LoginContainer>*/}
-					{/*<input */}
-						{/*type="text"*/}
-						{/*name="email"*/}
-						{/*placeholder="Email"*/}
-						{/*value={this.state.email}*/}
-						{/*onChange={event => this.setState({ [event.target.name]: event.targer.value })}*/}
-					{/*/>*/}
-					{/*<input */}
-						{/*type="password"*/}
-						{/*name="password"*/}
-						{/*placeholder="Email"*/}
-						{/*value={this.state.password}*/}
-						{/*onChange={event => this.setState({ [event.target.name]: event.targer.value })}*/}
-					{/*/>*/}
-					{/*<button onClick={() => this.handleLogin()}>Submit</button>*/}
-				{/*</LoginContainer>*/}
 			</div>
 		);
 	}
---
+++ rightnow-firebase/src/components/businessSignup.js
@@ -0,0 +1,14 @@
+import React, {Component}from 'react';
+
+class BussinessSignup extends Component{
+
+    render() {
+        return(
+            <div>
+                <h1>Business Home</h1>
+            </div>
+        );
+    }
+}
+
+export default BussinessSignup;
