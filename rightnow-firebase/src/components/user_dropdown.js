bb4f3e324b136f7fa294d912e3c6561bde6f66e3
jeffrey <jeffreyneilflynn@gmail.com>
2018-08-17 13:07:45 -0400

    new user dropdown file in components directory

---
+++ rightnow-firebase/src/components/user_dropdown.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import glamorous from "glamorous";
+
+
+export default class UserDropdown extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
