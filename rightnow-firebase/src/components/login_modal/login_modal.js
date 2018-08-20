ac2808cb4054a420ac831039b275ffeae6ee4eea
jeffrey <jeffreyneilflynn@gmail.com>
2018-08-20 07:10:34 -0400

    login modal component with styles

---
+++ rightnow-firebase/src/components/login_modal/login_modal.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Modal,
+  Header,
+  NormalSignIn,
+  Input,
+  Button,
+  Or,
+  OAuthContainer,
+  OAuthButton,
+  AuthLogo,
+  NewUser
+} from "./login_modal_styles";
+
+
+export default class SignInModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <Container>
+        <Modal>
+
+          <Header>Please sign in.</Header>
+          <NormalSignIn>
+            <Input type="text" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <Button>Sign In</Button>
+          </NormalSignIn>
+
+          <Or><span style={{ backgroundColor: "white", padding: "3%" }}>or</span></Or>
+
+          <OAuthContainer>
+            <OAuthButton>
+              <AuthLogo src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+              Login with Google
+            </OAuthButton>
+            <OAuthButton>
+              <AuthLogo src="https://upload.wikimedia.org/wikipedia/commons/c/c2/F_icon.svg" />
+              Login with Facebook
+            </OAuthButton>
+          </OAuthContainer>
+          
+          <NewUser>
+            <h1>Don't have an account?</h1>
+            <Button style={{ padding: "1.5%", fontSize: "1.5em" }}>Register Here</Button>
+          </NewUser>
+
+        </Modal>
+        {/* <CloseX onClick={() => this.props.closeModal()}>x</CloseX> */}
+      </Container>
+    )
+  }
+}
