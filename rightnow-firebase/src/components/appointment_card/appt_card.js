f4f6e8090f9d04d59e8a0b87c7d709432df14364
jeffrey <jeffreyneilflynn@gmail.com>
2018-08-20 12:37:54 -0400

    rendering 3 primary sections - image, info, and avail. appts.

--- rightnow-firebase/src/components/appointment_card/appt_card.js
+++ rightnow-firebase/src/components/appointment_card/appt_card.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import {
-  Container
+  Container,
+  BusinessImage,
+  BusinessInfo,
+  AvailableAppts
 } from "./appt_card_styles";
 
 
 export default class AppointmentCard extends Component {
   render() {
     return (
-      <Container>hey</Container>
+      <Container>
+        <BusinessImage src="https://nyoobserver.files.wordpress.com/2015/12/unnamed2.jpg" />
+        <BusinessInfo>
+          <h3>Caputo's Bake Shop</h3>
+        </BusinessInfo>
+        <AvailableAppts>
+          <h4>right meow</h4>
+        </AvailableAppts>
+      </Container>
     )
   }
 }
--- rightnow-firebase/src/components/appointment_card/appt_card_styles.js
+++ rightnow-firebase/src/components/appointment_card/appt_card_styles.js
@@ -1,6 +1,25 @@
-import glamorous from "glamorous";
+import glamorous, { P } from "glamorous";
 
 export const Container = glamorous.div({
   width: "15vw",
-  border: "1px solid gray"
+  border: "1px solid gray",
+  borderRadius: "5px",
+  display: "flex",
+  flexDirection: "column",
+});
+
+
+export const BusinessImage = glamorous.img({
+  maxWidth: "100%",
+  // maxHeight: "100px"
+});
+
+
+export const BusinessInfo = glamorous.div({
+  width: "100%"
+});
+
+
+export const AvailableAppts = glamorous.div({
+  width: "100%"
 });
