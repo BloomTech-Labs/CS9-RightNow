e11fe8e98ea7721fd7dff8b6f9a593ccb5539008
Henry K <mykim1891@hotmail.com>
2018-08-21 13:02:40 -0700

    Basic layout for Reg_forms

--- rightnow-firebase/src/components/register_modal/reg_forms_styles.js
+++ rightnow-firebase/src/components/register_modal/reg_forms_styles.js
@@ -1,2 +1,24 @@
 /* EXTENDED registration form */
 import glamorous from 'glamorous';
+
+export const FormContainer = glamorous.div({
+
+})
+export const Email = glamorous.div({
+
+})
+export const Name = glamorous.div({
+
+})
+export const PhoneNumber = glamorous.div({
+
+})
+export const Location = glamorous.div({
+
+})
+export const UpdateRadios = glamorous.div({
+
+})
+export const RegisterButton = glamorous.div({
+
+})
