1d821f66d3f068f2f3d327c77d9f74a94d7920dd
Harrison Brock <harrison@harrisonbrock.com>
2018-08-22 15:06:23 -0400

    ADDED button for user settings

--- rightnow-firebase/src/components/user_settings/user_settings_form.js
+++ rightnow-firebase/src/components/user_settings/user_settings_form.js
@@ -6,7 +6,24 @@ import glamorous from "glamorous";
 
 export const FormContainer = glamorous.div({
     // border: '1px solid blue',
-    width: '50%'
+    width: '50%',
+});
+
+const Button = glamorous.button({
+    borderRadius: '7px',
+    background: '#00c6fd',
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+    margin: '0 1%',
+    padding: '0 3%',
+    fontWeight: 600,
+    fontSize: '1.3em',
+    color: '#EBEBEB',
+    ':hover': {
+        cursor: 'pointer',
+        boxShadow: '2px 2px gray'
+    }
 });
 
 class UserSettings extends Component {
@@ -21,6 +38,7 @@ class UserSettings extends Component {
                     <UserNotification/>
                     <h1>Change Password</h1>
                     <UserChangePassword/>
+                    <Button>Save</Button>
                 </FormContainer>
             </div>
         );
