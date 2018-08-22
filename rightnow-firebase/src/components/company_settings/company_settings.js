6c9bea294d91680e3f08fe5f92e940086cf99697
Harrison Brock <harrison@harrisonbrock.com>
2018-08-22 17:38:18 -0400

    ADDED: Company Setting component

---
+++ rightnow-firebase/src/components/company_settings/company_settings.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CompanySettings extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CompanySettings;
