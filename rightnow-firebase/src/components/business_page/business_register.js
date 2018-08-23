84aefd0d1dceca065f84a6bc6e18abc813eb4044
Mark Marchant <jtla3@yahoo.com>
2018-08-23 15:23:12 -0700

    added dispaly for register button

--- rightnow-firebase/src/components/business_page/business_register.js
+++ rightnow-firebase/src/components/business_page/business_register.js
@@ -11,34 +11,51 @@ import {
 } from "./business-styles-account";
 
 export default class BusinessAccount extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayForms: true,
+      displaySuccess: false
+    };
+  }
+  submitForm = () => {
+    this.setState({ displayForms: false, displaySuccess: true })
+  }
+
   render() {
     return (
       <Container>
-        <h3>Business SignUp</h3>
-        <hr />
-        <Wrapper>
-          <LeftSide>
-            <label>First Name:</label>
-            <input type="text" required autocomplete="off" />
-            <label>Email:</label>
-            <input type="email" required autocomplete="off" />
-          </LeftSide>
+        {this.state.displayForms ? (
+          <div>
+            <h3>Business SignUp</h3>
+            <hr />
+            <Wrapper>
+              <LeftSide>
+                <label>First Name:</label>
+                <input type="text" required autocomplete="off" />
+                <label>Email:</label>
+                <input type="email" required autocomplete="off" />
+              </LeftSide>
 
-          <RightSide>
-            <label>Last Name:</label>
-            <input type="text" required autocomplete="off" />
-            <label>Phone Number:</label>
-            <input type="text" required autocomplete="off" />
-          </RightSide>
-        </Wrapper>
-
-        <Bottom>
-          <label>Google API</label>
-          <input type="text" placeholder="API" />
-        </Bottom>
-        <div>
-          <Button type="submit">Submit</Button>
-        </div>
+              <RightSide>
+                <label>Last Name:</label>
+                <input type="text" required autocomplete="off" />
+                <label>Phone Number:</label>
+                <input type="text" required autocomplete="off" />
+              </RightSide>
+            </Wrapper>
+            <Bottom>
+              <label>Google API</label>
+              <input type="text" placeholder="API" />
+            </Bottom>
+            <div>
+              <Button onClick={() => this.submitForm()} type="submit">Submit</Button>
+            </div>{" "}
+          </div>
+        ) : null}
+        {this.state.displaySuccess ? <h3>We got your application, thank you for the submission</h3> : null}
+     
       </Container>
     );
   }
