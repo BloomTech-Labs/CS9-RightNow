59d2bac9990160dac4f02b6d99a4587e69a76975
jeffrey <jeffreyneilflynn@gmail.com>
2018-08-23 11:09:46 -0400

    added react-google-places-component

--- rightnow-firebase/src/components/placesAPI/search_autocomplete.js
+++ rightnow-firebase/src/components/placesAPI/search_autocomplete.js
@@ -3,12 +3,16 @@ import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from 'react-places-autocomplete';
+import axios from "axios";
 
 
 export default class LocationSearchInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { address: '' };
+    this.state = { 
+      address: '',
+      place_id: ""
+    };
   }
 
   handleChange = address => {
@@ -16,13 +20,44 @@ export default class LocationSearchInput extends React.Component {
   };
 
   handleSelect = address => {
-    console.log(address);
+    let placeID;
     geocodeByAddress(address)
-      .then(results => console.log(results))
+      .then(results => {
+        console.log("RESULTS ~ ", results);
+        placeID = results[0].place_id;
+        console.log("PLACE ID ~ ", placeID);
+        return results;
+      })
       .then(results => getLatLng(results[0]))
+      .then(x => {
+        this.setState({ place_id: placeID });
+        return x;
+      })
+      .then(async x => {
+        await this.retrieve_details();
+        return x;
+      })
       .then(latLng => console.log('Success', latLng))
       .catch(error => console.error('Error', error));
   };
+
+  retrieve_details = () => {
+
+    const invocation = new XMLHttpRequest();
+    const url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJc4euylRYwokRnweuR3UzKYI&key=AIzaSyCbuJKo2ljNOXG4lboej16_P133vRRF4tU";
+
+    if (invocation) {
+      invocation.open('GET', url, true);
+      invocation.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+      invocation.onreadystatechange = xhttp => console.log(xhttp);
+      invocation.send();
+    }
+
+    // axios
+    //   .get(url)
+    //   .then(res => console.log("AXIOS RESPONSE ~ ", res))
+    //   .catch(err => console.log("AXIOS ERROR ~ ", err));
+  }
 
   render() {
     return (
