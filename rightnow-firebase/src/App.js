30503c0805e1a2b99c3f181291f7af49d414debd
Anthony <36117662+Anthony-Calderaro@users.noreply.github.com>
2018-08-23 19:17:56 -0400

    Revert "Business account"

--- rightnow-firebase/src/App.js
+++ rightnow-firebase/src/App.js
@@ -3,8 +3,7 @@ import NavBar from './components/nav_bar';
 import { init as firebaseInit } from './firebase/firebase';
 import UserProvider from './context/userContext';
 import { Route } from 'react-router-dom';
-import BusinessLanding from './components/business_page/business_landing';
-import BusinessAccount from './components/business_page/business_register';
+import BusinessLanding from './components/business_page/business_landing'
 import Customer from './components/customers/customers';
 import UserSettings from './components/user_settings/user_settings_form';
 import CompanySettings from './components/company_settings/company_settings';
@@ -21,8 +20,7 @@ class App extends Component {
             <div className="App">
                 <UserProvider>
                     <NavBar/>
-                    <Route exact path="/" component={Customer} />
-                    <Route exact path="/biz-account" component={BusinessAccount} />
+                    <Route exact path="/" component={Customer}/>
                     <Route exact path="/biz-landing" component={BusinessLanding}/>
                     <Route exact path="/user-settings" component={UserSettings}/>
                     <Route exact path="/company-settings" component={CompanySettings}/>
--- rightnow-firebase/src/components/business_page/business_landing.js
+++ rightnow-firebase/src/components/business_page/business_landing.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import * as routes from "../../constants/routes";
 import glamorous from "glamorous";
-import { Link } from "react-router-dom";
 
 import {
   Container,
@@ -26,22 +25,20 @@ export default class BusinessLanding extends Component {
           <TopLeft>TAKE ADVANTAGE</TopLeft>
           <MiddleLeft>OF YOUR BUSINESS</MiddleLeft>
           <BottomLeft>FULL POTENTIAL</BottomLeft>
-          <Link to="/biz-account">
-            <Button type="submit">REGISTER HERE</Button>
-          </Link>
+          <Button type="submit">REGISTER HERE</Button>
         </LeftSide>
 
         <RightSide>
           <TopRight>
-            <Text>POST YOUR AVAILABILITIES</Text>
-            <img />
+                    <Text>POST YOUR AVAILABILITIES</Text>
+                    <img />
           </TopRight>
-          <MiddleRight />
-          <Text2>MANAGE YOUR NEW BUSINESS</Text2>
-          <img />
-          <BottomRight>
-            <h3>IT'S THAT EASY!!!</h3>
-          </BottomRight>
+                <MiddleRight />
+                <Text2>MANAGE YOUR NEW BUSINESS</Text2>
+                <img />
+                <BottomRight>
+                    <h3>IT'S THAT EASY!!!</h3>
+                </BottomRight>
         </RightSide>
       </Container>
     );
