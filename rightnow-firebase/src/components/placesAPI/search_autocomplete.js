803fd5bae4a31c662d16c9d523265952e1cca4f9
jeffrey <jeffreyneilflynn@gmail.com>
2018-08-24 09:19:11 -0400

    IT WORKS

--- rightnow-firebase/src/components/placesAPI/search_autocomplete.js
+++ rightnow-firebase/src/components/placesAPI/search_autocomplete.js
@@ -1,105 +1,70 @@
-import React from 'react';
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
-import axios from "axios";
+//Import React Scrit Libraray to load Google object
+import React, { Component } from "react";
 
+class Search extends Component {
+  state = {
+    street_number: "",
+    street_name: "",
+    city: "",
+    state: "",
+    zip: "",
+    query: ""
+  };
 
-export default class LocationSearchInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      address: '',
-      place_id: ""
-    };
+  componentDidMount() {
+    // Locate input element
+    const input = document.getElementById("autocomplete");
+    // Initialize Google Autocomplete 
+    this.autocomplete = new window.google.maps.places.Autocomplete(input); 
+    // Fire Event when a suggested name is selected
+    this.autocomplete.addListener("place_changed", this.handlePlaceSelect); 
   }
 
-  handleChange = address => {
-    this.setState({ address });
-  };
 
-  handleSelect = address => {
-    let placeID;
-    geocodeByAddress(address)
-      .then(results => {
-        console.log("RESULTS ~ ", results);
-        placeID = results[0].place_id;
-        console.log("PLACE ID ~ ", placeID);
-        return results;
-      })
-      .then(results => getLatLng(results[0]))
-      .then(x => {
-        this.setState({ place_id: placeID });
-        return x;
-      })
-      .then(async x => {
-        await this.retrieve_details();
-        return x;
-      })
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error));
-  };
+  handlePlaceSelect = () => {
+    // Extract data
+    let place = this.autocomplete.getPlace();
+    console.log(place);
 
-  retrieve_details = () => {
+    // Define address (array of objects)
+    let address = place.address_components;
+    console.log(address);
 
-    const invocation = new XMLHttpRequest();
-    const url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJc4euylRYwokRnweuR3UzKYI&key=AIzaSyCbuJKo2ljNOXG4lboej16_P133vRRF4tU";
-
-    if (invocation) {
-      invocation.open('GET', url, true);
-      invocation.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-      invocation.onreadystatechange = xhttp => console.log(xhttp);
-      invocation.send();
+    // Check if address is valid
+    if (address) {
+      this.setState({ 
+        street_number: address[0].long_name, 
+        street_name: address[1].long_name,
+        city: address[2].long_name,
+        state: address[5].long_name,
+        zip: address[6].long_name,
+        query: place.formatted_address 
+      });
     }
-
-    // axios
-    //   .get(url)
-    //   .then(res => console.log("AXIOS RESPONSE ~ ", res))
-    //   .catch(err => console.log("AXIOS ERROR ~ ", err));
   }
 
   render() {
     return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => {
-          console.log(getInputProps)
-          return (<div style={{margin: "10%", fontSize: "1.5em"}}>
-            <input style={{padding: "2%", fontSize: "1.5em"}}
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input',
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}}
-      </PlacesAutocomplete>
+      <div>
+
+        <input 
+          id="autocomplete" 
+          placeholder="Enter business location" 
+          intText="Search City"
+          name="query"
+          value={this.state.query}
+          onChange={e => this.setState({ [e.target.name]: e.target.value })}
+          style={{
+            margin: '15% auto',
+            width: 800,
+            padding: "3%",
+            fontSize: "2em"
+          }}
+        />
+
+      </div>
     );
   }
 }
+
+export default Search;
