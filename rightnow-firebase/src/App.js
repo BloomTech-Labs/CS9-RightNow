81a2da225d7395cfeafaa4296ebebaa1c6866ca7
Anthony <anthony@anthonys-mbp.lan>
2018-08-24 12:54:54 -0400

    merge conflicts part 2

--- rightnow-firebase/src/App.js
+++ rightnow-firebase/src/App.js
@@ -1,39 +1,5 @@
 import React, { Component } from 'react';
 import NavBar from './components/nav_bar';
-<<<<<<< HEAD
-import UserLanding from './components/user_landing';
-import { init as firebaseInit } from './firebase/firebase';
-database = init.firestore();
-import FeaturedAppointments from './components/featured_appointments/feat_appts';
-import IndustryView from './components/industry_view/industry_view';
-import UserProvider from './context/userContext';
-// import Biz_Appointments from './components/view_appointments_business/appointments_business';
-// import Biz_Appointments from './view_appointments_business/appointments_business.js';
-import Biz_Appointments from './appointments_business/appointments_business.js';
-import { database } from 'firebase';
-
-class App extends Component {
-	constructor() {
-		super();
-		firebaseInit();
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<UserProvider>
-					<NavBar />
-					<UserLanding />
-					<FeaturedAppointments />
-					<IndustryView />
-				</UserProvider>
-
-				<Biz_Appointments />
-			</div>
-		);
-	}
-=======
 import { init as firebaseInit } from './firebase/firebase';
 import UserProvider from './context/userContext';
 import { Route } from 'react-router-dom';
@@ -64,7 +30,6 @@ class App extends Component {
             </div>
         );
     }
->>>>>>> d00f4a2cbac6d6558dc4e3a22f113a8b486e69d3
 }
 export default App;
 
