a7c3308e56bf788c86e25507b2829c203ec3aed9
Mark Marchant <jtla3@yahoo.com>
2018-08-24 14:02:23 -0700

    commit fix

--- rightnow-firebase/src/App.js
+++ rightnow-firebase/src/App.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import NavBar from './components/nav/nav_bar';
-import { init as firebaseInit } from './firebase/firebase';
-import UserProvider from './context/userContext';
-import { Route } from 'react-router-dom';
-import BusinessLanding from './components/business_page/business_landing';
-import BusinessAccount from './components/business_page/business_register';
-import Customer from './components/customers/customers';
-import UserSettings from './components/user_settings/user_settings_form';
-import CompanySettings from './components/company_settings/company_settings';
-import Biz_Appointments from './appointments_business/appointments_business';
+import React, { Component } from "react";
+import NavBar from "./components/nav/nav_bar";
+import { init as firebaseInit } from "./firebase/firebase";
+import UserProvider from "./context/userContext";
+import { Route } from "react-router-dom";
+import BusinessLanding from "./components/business_page/business_landing";
+import BusinessAccount from "./components/business_page/business_register";
+import Customer from "./components/customers/customers";
+import UserSettings from "./components/user_settings/user_settings_form";
+import CompanySettings from "./components/company_settings/company_settings";
+import Biz_Appointments from "./appointments_business/appointments_business";
 
 class App extends Component {
-    constructor() {
-        super();
-        firebaseInit();
-        this.state = {};
-    }
+  constructor() {
+    super();
+    firebaseInit();
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <UserProvider>
-                    <NavBar/>
-                    <Route exact path="/" component={Customer} />
-                    <Route exact path="/biz-account" component={BusinessAccount} />
-                    <Route exact path="/biz-landing" component={BusinessLanding}/>
-                    <Route exact path="/user-settings" component={UserSettings}/>
-                    <Route exact path="/company-settings" component={CompanySettings}/>
-                </UserProvider>
-
-                <Biz_Appointments />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <UserProvider>
+          <NavBar />
+          <Route exact path="/" component={Customer} />
+          <Route exact path="/biz-account" component={BusinessAccount} />
+          <Route exact path="/biz-landing" component={BusinessLanding} />
+          <Route exact path="/user-settings" component={UserSettings} />
+          <Route exact path="/company-settings" component={CompanySettings} />
+        </UserProvider>
+      </div>
+    );
+  }
 }
 export default App;
-
