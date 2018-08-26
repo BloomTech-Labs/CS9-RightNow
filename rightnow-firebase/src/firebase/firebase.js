75142bd1594401e09c8119fe95afe17d9e6f42a2
Jeffrey Flynn <jeffreyneilflynn@gmail.com>
2018-08-25 22:16:27 -0400

    removed some commented out code from firebase.js

--- rightnow-firebase/src/firebase/firebase.js
+++ rightnow-firebase/src/firebase/firebase.js
@@ -45,27 +45,6 @@ db.enablePersistence().catch(err => console.log("PERSISTANCE ERROR: ", err));
 auth.onAuthStateChanged(async currentUser => {
   console.log("current user status has changed");
   console.log("current user is: ", currentUser);
-
-  // THIS WORKS
-  // const newAppt = await createNewAppointment({
-  //   active: true,
-  //   details: {
-  //     time: "3:30 - 5:30",
-  //     place: "some location",
-  //     cost: "$400",
-  //     type: "haircut"
-  //   },
-  //   businessId: "xGXdWn7l2mQWiLiVziOv4zJqsGi2"
-  // });
-  // console.log(newAppt);
-
-  // THIS WORKS
-  // const fresh = await newFutureAppointment("xGXdWn7l2mQWiLiVziOv4zJqsGi2", "mEHyeJ7nsIcsYWh9Jrqa");
-  // console.log(fresh);
-
-  // THIS WORKS
-  const confirm = await customerConfirmsAppt("e4WqE9gsGOy7XVi3jesx", "GcvSXEc5IavQ8pT0FUJM");
-  console.log(confirm);
 });
 
 
