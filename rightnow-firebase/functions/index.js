f0efd34a1014f23a1a5bd84c4b0cdf7ecc9e5422
jeffrey <jeffreyneilflynn@gmail.com>
2018-08-26 13:45:26 -0400

    delete instances of an appointment once deleted from primary appointments collection

--- rightnow-firebase/functions/index.js
+++ rightnow-firebase/functions/index.js
@@ -6,19 +6,20 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 
+
 /*
   Notes: 
 
     functions.firestore.document()
 
 
-    @param snap = query document snapshot
+    @param snap -- query document snapshot
 
-      * snap.ref = reference
-      * snap.data() = data that triggered the event
+      * snap.ref -- reference
+      * snap.data() -- data that triggered the event
     
 
-    @param context = basic infor about the triggered event
+    @param context -- basic infor about the triggered event
 
       * .params -- object of wildcard key-value pairs
 
@@ -34,6 +35,14 @@ admin.initializeApp();
   collection.
 
   @returns document reference to the new future_appointments doc
+
+  @logic
+    1. business submits new appointment that follows appointment data structure outline
+    2. function gets triggered
+    3. function adds a new document to the business' future_appointments collection
+        -- the id of the new document is the same id as it is in the appointments primary collection
+    4. function returns a document reference to the newly added appointment 
+        -- not sure if the client can gain access
 */
 
 exports.handleNewAppointment = functions.firestore
@@ -41,13 +50,40 @@ exports.handleNewAppointment = functions.firestore
   .onCreate((snap, context) => {
     const appointmentRef = snap.ref;
     const businessRef = snap.data().business_ref; // id for testing
-
     const db = admin.firestore();
+
+    db.settings({ timestampsInSnapshots: true });
 
     db
       .doc(`/business_test/${businessRef}`)
       .collection("future_appointments")
-      .add({ appointment_ref: appointmentRef })
+      .doc(context.params.apptId)
+      .set({ appointment_ref: appointmentRef })
       .then(busnDocRef => busnDocRef)
+      .catch(err => console.log("error", err));
+  });
+
+
+
+/*
+  When an appointment gets deleted by a business, this method will search for identical
+  references in the business' future_appointments or past_appointments collection, depending
+  on the value of the appointment's `active` property.
+
+  @returns nothing
+*/
+exports.handleDeleteAppointment = functions.firestore
+  .document(`/appt_test/{apptId}`)
+  .onDelete((snap, context) => {
+    const isActive = snap.data().active;
+    const businessRef = snap.data().business_ref;
+    const db = admin.firestore();
+
+    db.settings({ timestampsInSnapshots: true });
+
+    db
+      .doc(`business_test/${businessRef}/${isActive ? "future_appointments" : "past_appointments"}/${context.params.apptId}`)
+      .delete()
+      .then(() => console.log("success"))
       .catch(err => console.log("error", err));
   });
