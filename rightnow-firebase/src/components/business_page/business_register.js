e52b7020fe150e07692e196ca06d0380ac28f94e
Anthony <anthony@anthonys-mbp.lan>
2018-08-27 15:13:07 -0400

    Register New Business User
    On submit, firebase will store the email, password, and user id in the user database under authentication tab

--- rightnow-firebase/src/components/business_page/business_register.js
+++ rightnow-firebase/src/components/business_page/business_register.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import glamorous from "glamorous";
 import PlacesAPI from "../placesAPI/search_autocomplete";
-import { registerUser, getUserId } from "../../firebase/db_interact";
+import './business_register_styles.css';
+import { registerUser, getUserId, createUser } from "../../firebase/db_interact";
 import {
   Container,
   LeftSide,
@@ -10,40 +11,30 @@ import {
   Wrapper,
   Button
 } from "./business-styles-account";
+// import firebase, { auth } from '../../firebase/firebase.js';
+import { doCreateUserWithEmailAndPassword } from '../../firebase/auth';
+
+
 
 export default class BusinessAccount extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayForms: true,
-      displaySuccess: false,
-      email: "",
+      displayForms: true, // what does this do? 
+      // displaySuccess: false, // what does this do?
       first_name: "",
       last_name: "",
-      phone: ""
+      email: "",
+      password: "",
+      phone: "",
+      company: ""
     };
   }
-
-  submitForm = async () => {
-    const userId = await getUserId();
-
-    const owner = {
-      first_name: this.state.first_name,
-      last_name: this.state.last_name,
-      email: this.state.email,
-      phone: this.state.phone
-    };
-
-    const business = this.props.value.data.business;
-
-    const allData = {
-      uid: userId,
-      business_information: business,
-      owner_information: owner
-    };
-
-    registerUser("busn_ACTUAL", allData);
-  };
+  createUser = () => {
+    // console.log('inside createUser');
+    const { email, password } = this.state;
+    doCreateUserWithEmailAndPassword(email, password);
+  }
 
   render() {
     console.log(this.props);
@@ -56,54 +47,76 @@ export default class BusinessAccount extends Component {
             <Wrapper>
 
               <LeftSide>
-                <label>First Name:</label>
-                <input
-                  type="text"
-                  name="first_name"
-                  onChange={e => this.setState({ [e.target.name]: e.target.value })}
-                  value={this.state.first_name}
-                  required
-                  autocomplete="off"
-                />
-                <label>Email:</label>
-                <input
-                  type="email"
-                  onChange={e => this.setState({ [e.target.name]: e.target.value })}
-                  name="email"
-                  value={this.state.email}
-                  required
-                  autocomplete="off"
-                />
+
+                <form>
+                  <label>First Name:</label>
+                  <input
+                    type="text"
+                    name="first_name"
+                    onChange={e => this.setState({ [e.target.name]: e.target.value })}
+                    value={this.state.first_name}
+                    required
+                    autocomplete="off"
+                  />
+                  <label>Email:</label>
+                  <input
+                    type="email"
+                    onChange={e => this.setState({ [e.target.name]: e.target.value })}
+                    name="email"
+                    value={this.state.email}
+                    required
+                    autocomplete="off"
+                  />
+                  <label>Password:</label>
+                  <input
+                    type="password"
+                    onChange={e => this.setState({ [e.target.name]: e.target.value })}
+                    name="password"
+                    value={this.state.password}
+                    required
+                    autocomplete="off"
+                  />
+                  <label>Last Name:</label>
+                  <input
+                    type="text"
+                    onChange={e => this.setState({ [e.target.name]: e.target.value })}
+                    name="last_name"
+                    value={this.state.last_name}
+                    required
+                    autocomplete="off"
+                  />
+                  <label>Phone Number:</label>
+                  <input
+                    type="text"
+                    onChange={e => this.setState({ [e.target.name]: e.target.value })}
+                    name="phone"
+                    value={this.state.phone}
+                    required
+                    autocomplete="off"
+                  />
+                  <label>Company Name:</label>
+                  <input
+                    type="text"
+                    onChange={e => this.setState({ [e.target.name]: e.target.value })}
+                    name="company"
+                    value={this.state.company}
+                    required
+                    autocomplete="off"
+                  />
+                  <Button onClick={this.createUser}>Submit</Button>
+                </form>
               </LeftSide>
 
               <RightSide>
-                <label>Last Name:</label>
-                <input
-                  type="text"
-                  onChange={e => this.setState({ [e.target.name]: e.target.value })}
-                  name="last_name"
-                  value={this.state.last_name}
-                  required
-                  autocomplete="off"
-                />
-                <label>Phone Number:</label>
-                <input
-                  type="text"
-                  onChange={e => this.setState({ [e.target.name]: e.target.value })}
-                  name="phone"
-                  value={this.state.phone}
-                  required
-                  autocomplete="off"
-                />
               </RightSide>
             </Wrapper>
 
             
-              <Bottom>
+              {/* <Bottom>
                 <label>Google API</label>
                 <PlacesAPI busnContext={this.props.value} />
-                <Button onClick={() => this.submitForm()}>Submit</Button>
-              </Bottom>
+                
+              </Bottom> */}
 
           </div>
         ) : null}
