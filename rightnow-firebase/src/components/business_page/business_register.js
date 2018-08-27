dab58af79d478ed5dd00452936def8a9f3367fe2
Anthony <anthony@anthonys-mbp.lan>
2018-08-27 19:44:23 -0400

    Ready for Pull

--- rightnow-firebase/src/components/business_page/business_register.js
+++ rightnow-firebase/src/components/business_page/business_register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import glamorous from "glamorous";
 import PlacesAPI from "../placesAPI/search_autocomplete";
 import "./business_register_styles.css";
@@ -23,14 +24,14 @@ export default class BusinessAccount extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // displaySuccess: false, // what does this do?
+      displaySuccess: false, // what does this do?
       displayForms: true, // what does this do?
       email: "",
       password: "",
       business: "",
       first_name: "",
       last_name: "",
-      phone: ""
+      phone: "",
     };
   }
 
@@ -43,8 +44,7 @@ export default class BusinessAccount extends Component {
     // Creates a new user, password, user id in firestore (console > authentication > users)
     const { email, password, first_name, last_name, phone, business } = this.state;
     doCreateUserWithEmailAndPassword(email, password);
-
-
+  
     // Create user data on business db
     db.collection("biz") // Collection name per Firestore
       .add({
@@ -52,17 +52,12 @@ export default class BusinessAccount extends Component {
         employee_last: last_name,
         phone: phone,
         business: business
-      })
-      .catch(() => {
-        console.log("failure");
-      });    
+      })   
   };
-
 
   render() {
     return (
       <Container>
-        {this.state.displayForms ? (
           <div>
             <h3>Business SignUp</h3>
             <hr />
@@ -123,7 +118,9 @@ export default class BusinessAccount extends Component {
                     required
                     autocomplete="off"
                   />
-                  <Button onClick={this.createUser()}>Submit</Button>
+                  <Link to="/">
+                    <Button onClick={this.createUser()}>Submit</Button>
+                  </Link>
                 </form>
               </LeftSide>
 
@@ -136,9 +133,6 @@ export default class BusinessAccount extends Component {
                 
               </Bottom> */}
           </div>
-        ) : null}
-
-        {this.state.displaySuccess ? <h3>We got your application, thank you for the submission</h3> : null}
       </Container>
     );
   }
