bd2478bfa06c2e068f9e1d369799cc8da2f82fe6
jeffrey <jeffreyneilflynn@gmail.com>
2018-09-01 16:05:41 -0400

    business login page

--- rightnow-firebase/src/components/business_page/business_register.js
+++ rightnow-firebase/src/components/business_page/business_register.js
@@ -18,11 +18,9 @@ import {
 	LoginButton,
 	LeftSide,
 	RightSide,
-	Input,
 	Bottom,
 	TopWrapper,
 	BottomWrapper,
-	Button
 } from './business_register_styles';
 
 
@@ -42,7 +40,6 @@ const LandingContainer = glamorous.div({
 	position: "absolute",
 	height: "100vh",
 	width: "100vw",
-	top: "30%",
 	background: "transparent",
 	transition: "all 1.2s"
 });
@@ -52,6 +49,7 @@ const WelcomePage = glamorous.div({
 	position: "absolute",
 	height: "100vh",
 	width: "100vw",
+	top: "30%",
 	background: "transparent",
 	transition: "all 1.2s",
 });
@@ -64,6 +62,52 @@ const FormContainer = glamorous.div({
 	top: "100vh",
 	background: "transparent",
 	transition: "all 1.2s",
+});
+
+
+const Container = glamorous.div({
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  width: '100%',
+  height: '25%',
+  margin: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignContent: 'center'
+});
+
+
+const Input = glamorous.input({
+  margin: '1% auto',
+  width: '23%',
+  padding: '.7% 0',
+	border: 'none',
+	borderRadius: "5px",
+  fontSize: '1.3rem',
+  fontWeight: 600,
+  color: 'white',
+	textAlign: 'center',
+	textShadow: "1px 1px black",
+	backgroundColor: "rgba(225, 225, 225, 0.4)"
+});
+
+
+const Button = glamorous.button({
+	width: '21%',
+	margin: "auto",
+	padding: "0.7% 0",
+  color: 'white',
+	fontWeight: 600,
+	fontSize: "1.3em",
+  border: '0.3px solid white',
+  borderRadius: '5px',
+  backgroundColor: 'transparent',
+  ':hover': { color: 'white', backgroundColor: 'rgba(225, 225, 225, 0.1)', cursor: 'pointer' },
+  ':focus': { color: 'white', backgroundColor: 'rgba(225, 225, 225, 0.1)' }
 });
 
 
@@ -136,10 +180,8 @@ class BusinessAccount extends Component {
 	}
 
 	handleLoginDisplay = () => {
-		const landingContainer = document.querySelector("#landing");
-		const loginContainer = document.querySelector("#login");
-		landingContainer.style.top = "-100vh";
-		loginContainer.style.top = 0;
+		document.querySelector("#landing").style.top = "-100vh";
+		document.querySelector("#login").style.top = 0;
 	}
 
 	render() {
@@ -148,129 +190,123 @@ class BusinessAccount extends Component {
 		
 		return (
 			<FixedContainer>
-
-					<LandingContainer >
-
-
-						<WelcomePage id="landing">
-							<Title>
-								<TitleBorder>Sesho: Manager</TitleBorder>
-								<Description>MANAGE YOUR APPOINTMENTS WITH A SIMPLE SESSION OF SESHO</Description>
-							</Title>
-
-							<ButtonContainer>
-								<RegButton onClick={() => this.handleRegisterDisplay()}>
-										Register
-								</RegButton>
-								<CenterLine />
-								<LoginButton onClick={() => this.handleLoginDisplay()}>
-									Login
-								</LoginButton>
-							</ButtonContainer>
-						</WelcomePage>
-					
-				
+				<LandingContainer id="swoosh" >
 
 
-				<FormContainer id="register">
-					<div>Already a family of Sesho? click here</div>
-					<TopWrapper>
-						<LeftSide>
-							<div>First Name:</div>
+					<WelcomePage id="landing">
+						<Title>
+							<TitleBorder>Sesho: Manager</TitleBorder>
+							<Description>MANAGE YOUR APPOINTMENTS WITH A SIMPLE SESSION OF SESHO</Description>
+						</Title>
+
+						<ButtonContainer>
+							<RegButton onClick={() => this.handleRegisterDisplay()}>
+									Register
+							</RegButton>
+							<CenterLine />
+							<LoginButton onClick={() => this.handleLoginDisplay()}>
+								Login
+							</LoginButton>
+						</ButtonContainer>
+					</WelcomePage>
+
+
+					<FormContainer id="register">
+						<div>Already a family of Sesho? click here</div>
+						<TopWrapper>
+							<LeftSide>
+								<div>First Name:</div>
+								<Input
+									type="text"
+									name="first_name"
+									placeholder="First Name"
+									onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+									value={this.state.first_name}
+									required
+									autocomplete="off"
+								/>
+							</LeftSide>
+
+							<RightSide>
+								<div>Last Name:</div>
+								<Input
+									type="text"
+									onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+									name="last_name"
+									placeholder="Last Name"
+									value={this.state.last_name}
+									required
+									autocomplete="off"
+								/>
+							</RightSide>
+						</TopWrapper>
+
+						<BottomWrapper>
+							<div>Email:</div>
 							<Input
-								type="text"
-								name="first_name"
-								placeholder="First Name"
+								type="email"
 								onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
-								value={this.state.first_name}
+								name="email"
+								placeholder="Email"
+								value={this.state.email}
 								required
 								autocomplete="off"
 							/>
-						</LeftSide>
-
-						<RightSide>
-							<div>Last Name:</div>
+							<div>Password</div>
 							<Input
-								type="text"
+								type="password"
 								onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
-								name="last_name"
-								placeholder="Last Name"
-								value={this.state.last_name}
+								name="password"
+								placeholder="Password"
+								value={this.state.password}
 								required
 								autocomplete="off"
 							/>
-						</RightSide>
-					</TopWrapper>
-
-					<BottomWrapper>
-						<div>Email:</div>
-						<Input
-							type="email"
-							onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
-							name="email"
-							placeholder="Email"
-							value={this.state.email}
-							required
-							autocomplete="off"
-						/>
-						<div>Password</div>
-						<Input
-							type="password"
-							onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
-							name="password"
-							placeholder="Password"
-							value={this.state.password}
-							required
-							autocomplete="off"
-						/>
-						<div>Phone Number:</div>
-						<Input
-							type="text"
-							onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
-							name="phone"
-							placeholder="Phone Number"
-							value={this.state.phone}
-							required
-							autocomplete="off"
-						/>
-						<Bottom>
-							<div>Google API</div>
-							<PlacesAPI busnContext={this.props.value} />
-						</Bottom>
-					</BottomWrapper>
-					<Button onClick={() => this.submitForm()}>Submit</Button>
-				</FormContainer>
+							<div>Phone Number:</div>
+							<Input
+								type="text"
+								onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+								name="phone"
+								placeholder="Phone Number"
+								value={this.state.phone}
+								required
+								autocomplete="off"
+							/>
+							<Bottom>
+								<div>Google API</div>
+								<PlacesAPI busnContext={this.props.value} />
+							</Bottom>
+						</BottomWrapper>
+						<Button onClick={() => this.submitForm()}>Submit</Button>
+					</FormContainer>
 
 
-				<FormContainer id="login">
-					<div>Not a family of Sesho? click here</div>
-					<BottomWrapper>
-						<div>Email:</div>
-						<Input
-							type="email"
-							onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
-							name="email"
-							placeholder="Email"
-							value={this.state.email}
-							required
-							autocomplete="off"
-						/>
-						<div>Password</div>
-						<Input
-							type="password"
-							onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
-							name="password"
-							placeholder="Password"
-							value={this.state.password}
-							required
-							autocomplete="off"
-						/>\
+					<FormContainer id="login">
+						<Container>
+							<Input
+								type="email"
+								onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+								name="email"
+								placeholder="Email"
+								value={this.state.email}
+								required
+								// autocomplete="off"
+							/>
+							<Input
+								type="password"
+								onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+								name="password"
+								placeholder="Password"
+								value={this.state.password}
+								required
+								// autocomplete="off"
+							/>\
 
-						<button onClick={() => this.handleEmailSignIn()}>Login</button>
-					</BottomWrapper>
-				</FormContainer>
+							<Button onClick={() => this.handleEmailSignIn()}>Login</Button>
+						</Container>
+					</FormContainer>
 
-				{this.state.displaySuccess ? <h3>We got your application, thank you for the submission</h3> : null}
+					{this.state.displaySuccess ? <h3>We got your application, thank you for the submission</h3> : null}
 				</LandingContainer>
 			</FixedContainer>
 		);
