340b97053c7759ea92256e1c423d6a379130f27c
jeffrey <jeffreyneilflynn@gmail.com>
2018-09-01 10:37:19 -0400

    all timeframes rendering

--- rightnow-firebase/src/components/business_appointments/calendar.js
+++ rightnow-firebase/src/components/business_appointments/calendar.js
@@ -1,4 +1,4 @@
-import 'rc-select/assets/index.css';
+// import 'rc-select/assets/index.css';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import React, { Component } from "react";
@@ -10,10 +10,15 @@ import moment from 'moment';
 Calendar.setLocalizer(Calendar.momentLocalizer(moment));
 
 
+const today = new Date();
+const minTime = new Date(moment().hour(4).minute(0));
+const maxTime = new Date(moment().hour(23).minute(59));
+
+
 export default class BusnCalendar extends Component {
   state = {
     events: [
-      { start: new Date(), end: new Date(moment().add(1, "days")), title: "Some title" }
+      { start: new Date(), end: new Date(moment().add(1, "hours")), title: "Haircut with Joe" }
     ]
   }
   
@@ -22,9 +27,14 @@ export default class BusnCalendar extends Component {
       <div>
 
         <Calendar 
-          defaultDate={new Date()}
+          defaultDate={today}
           defaultView="week"
           events={this.state.events}
+          step={15} // 6 steps * 10 timeslots = 60min intervals for inter-week 
+          timeslots={4}
+          min={minTime}
+          max={maxTime}
+          style={{ height: "100vh" }}
         />
 
       </div>
