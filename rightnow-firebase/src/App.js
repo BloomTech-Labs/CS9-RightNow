4134719f350f4e78fbea63cdde539be7c458a140
Anthony <anthony@anthonys-mbp.lan>
2018-09-03 15:02:54 -0400

    Removed components from appjs

--- rightnow-firebase/src/App.js
+++ rightnow-firebase/src/App.js
@@ -7,7 +7,6 @@ import Customer from './components/customers/customers';
 import UserSettings from './components/user_settings/user_settings_form';
 import CompanySettings from './components/company_settings/company_settings';
 import BusnApptsView from "./components/business_appointments/busn_appts";
-import AppointmentDetails from "./components/appointmentDetails/appointmentDetailsCustomerView";
 
 class App extends Component {
 	render() {
