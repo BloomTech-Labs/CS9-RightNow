a3a96ca6e66408cb3e4dec2bf71cbc1520cfd332
Anthony <anthony@anthonys-mbp.lan>
2018-09-04 15:08:54 -0400

    Appointment card shows appt info and delete works

--- rightnow-firebase/src/components/business_appointments/appointmentDetails/appointmentDetailsCustomerView.js
+++ rightnow-firebase/src/components/business_appointments/appointmentDetails/appointmentDetailsCustomerView.js
@@ -9,6 +9,7 @@ class AppointmentDetails extends Component {
     } 
     
     onDeleteClick = () => {
+        // console.log(this.props.busnContext.selectedItem.id)
         axios.delete(`https://us-central1-cs9-rightnow.cloudfunctions.net/haveAsesh/appointment/${this.props.busnContext.selectedItem.id}`)
             .then(res => {
                 console.log(res, "success");
@@ -21,7 +22,7 @@ class AppointmentDetails extends Component {
             <div>
                 <div className="Detail__Container">
                     <div className='content'>
-                            <i onClick={() => this.onDeleteClick()} class="far delete fa-trash-alt"></i>
+                            <div onClick={() => this.onDeleteClick()}><i class="far delete fa-trash-alt"></i></div>
                             <h3 className="Detail__header">Sesh Details</h3>
                         <div className="Detail__content">
                             <div>
@@ -30,7 +31,7 @@ class AppointmentDetails extends Component {
                             <div><i class="far icon fa-clock"></i><div className="apptProps">{`${moment(this.props.busnContext.start).format('LLL')} --- ${moment(this.props.busnContext.end).format('LLL')}`}</div></div>
                             {/* <div><i class="far icon fa-calendar-plus"></i><div className="apptProps">{this.props.busnContext.service}</div></div> */}
                             <div>
-                                <i class="fas icon fa-user-check"></i><div className="apptProps">{this.props.busnContext.selectedItem.customer_ref}</div> {/* user-times */}
+                                <i class="fas icon fa-user-check"></i><div className="apptProps">{this.props.busnContext.personal.full_name}</div> {/*{this.props.busnContext.selectedItem.customer_ref} */}
                             </div>
                             <div>
                                 <i class="far icon fa-money-bill-alt"></i><div className="apptProps">{this.props.busnContext.selectedItem.cost}</div>
